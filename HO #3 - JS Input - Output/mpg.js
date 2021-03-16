f6770d7a505c5daa01d85a907d9a250f0997f960
mad2026 <77991214+mad2026@users.noreply.github.com>
2021-03-16 14:30:01 -0500

    adding assingment HO #3

---
+++ HO #3 - JS Input - Output/mpg.js
@@ -0,0 +1,5 @@
+var floatMiles, floatGallons, mpg;
+     floatMiles = parseFloat(prompt("Enter miles driven"));
+     floatGallons = parseFloat(prompt("Enter gallons of gas used"));
+     mpg = floatMiles/floatGallons;
+     alert("Miles per gallon = " + mpg);
